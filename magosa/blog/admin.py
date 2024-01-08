2a15c2596f776565f4333a51af9c4792601076f5
Berke Can Akkor <berkecan2323@icloud.com>
2024-01-08 14:47:15 +0200

    end of the project

--- magosa/blog/admin.py
+++ magosa/blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from blog.models import contactFormModel
 # Register your models here.
+
+
+class contactFormModelAdmin(admin.ModelAdmin):
+    list_display = ('username','message')
+
+admin.site.register(contactFormModel,contactFormModelAdmin)
--- magosa/blog/models.py
+++ magosa/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class contactFormModel(models.Model):
+    username = models.CharField(max_length=50, verbose_name="Adınız soyadınız")
+    message = models.TextField(max_length=200, verbose_name="Mesajınız")
+    
+    def __str__(self):
+        return self.username
