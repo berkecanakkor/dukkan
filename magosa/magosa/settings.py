e88ede0cee46877f3baeaa2b0d486ae184ddcba8
Berke Can Akkor <berkecan2323@icloud.com>
2024-01-09 10:13:47 +0200

    end of the project

--- magosa/magosa/settings.py
+++ magosa/magosa/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,12 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ovd-t79n)8n3vzh=h)51kk#+j$2ako7spf)^2kgg+=0#o@k7^2"
+#SECRET_KEY = "django-insecure-ovd-t79n)8n3vzh=h)51kk#+j$2ako7spf)^2kgg+=0#o@k7^2"
+SECRET_KEY = getenv("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv("IS_DEVELOPMENT", True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    getenv("APP_HOST")
+]
 
 
 # Application definition
@@ -121,6 +126,8 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS=[
     BASE_DIR / "static"
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Default primary key field type
